418655d4eae9c747d0c673aa8ac222c18563b560
Credence <erichamby83@gmail.com>
2022-11-03 13:17:12 -0400

    Create onStart2.lua
    Signed-off-by: Credence <erichamby83@gmail.com>

---
+++ source/system/onStart2.lua
@@ -0,0 +1,8 @@
+Material = "Storage" --export: (Default: Storage) Ore Name
+BG_Color = "15/255,24/255,29/255" --export: (Default: 15/255,24/255,29/255 (DU/UI))
+WallPaper = "assets.prod.novaquark.com/130546/b6fc96cb-c570-4554-901f-099711f5383d.jpg" --export: Wallpapper Image
+UseWallpaper = false --export: Uses wallpaper
+HideUnit = true --export: Hides the bottom right widget
+showJericoBar = true --export: Header Bar - true or false
+showinKL = false --export: Show in KL or L
+version = '2.1'
