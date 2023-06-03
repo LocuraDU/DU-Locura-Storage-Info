6e5c3b660495f9ef487326fdd2f17fbcea72559a
CredenceHamby <erichamby83@gmail.com>
2023-06-02 20:18:38 -0400

    v2.5
    Signed-off-by: CredenceHamby <erichamby83@gmail.com>

---
+++ source/system/onStart.lua
@@ -0,0 +1,49 @@
+-- Set options for storage and screens
+Material = "TEXT1" --export: Screen #1 <hr> Select the main title for screen #1
+itemID = 2125213321 --export: Screen #1 <hr> The item ID for screen #1 <br>ID found at du-lua.dev/#/items
+Value = '1' --export: Screen #1 <hr> Select the value you want for screen #1
+ContSize = 1500 --export: Screen #1 <hr> The total container size for screen #1
+ReverseRGB = true --export: Screen #1 <hr> Select the RGB bar direction for screen #1
+BG_Color = "15/255,24/255,29/255" --export: Screen #1 <hr> Select the background color for screen #1
+Material2 = "TEXT2" --export: Screen #2 <hr> Select the main title for screen #2
+itemID2 = 521274609 --export: Screen #2 <hr> The item ID for screen #2 <br>ID found at du-lua.dev/#/items
+Value2 = '1' --export: Screen #2 <hr> Select the value you want for screen #2
+ContSize2 = 1500 --export: Screen #2 <hr> The total container size for screen #2
+ReverseRGB2 = false --export: Screen #2 <hr> Select the RGB bar direction for screen #2
+BG_Color2 = "15/255,24/255,29/255" --export: Screen #2 <hr> Select the background color for screen #2
+Material3 = "TEXT3" --export: Screen #3 <hr> Select the main title for screen #3
+itemID3 = 1832899704 --export: Screen #3 <hr> The item ID for screen #3 <br>ID found at du-lua.dev/#/items
+Value3 = '1' --export: Screen #3 <hr> Select the value you want for screen #3
+ContSize3 = 1500 --export: Screen #3 <hr> The total container size for screen #3
+ReverseRGB3 = true --export: Screen #3 <hr> Select the RGB bar direction for screen #3
+BG_Color3 = "15/255,24/255,29/255" --export: Screen #3 <hr> Select the background color for screen #3
+Material4 = "TEXT4" --export: Screen #4 <hr> Select the main title for screen #4
+itemID4 = 2125213321 --export: Screen #4 <hr> The item ID for screen #4 <br>ID found at du-lua.dev/#/items
+Value4 = '1' --export: Screen #4 <hr> Select the value you want for screen #4
+ContSize4 = 1500 --export: Screen #4 <hr> The total container size for screen #4
+ReverseRGB4 = true --export: Screen #4 <hr> Select the RGB bar direction for screen #4
+BG_Color4 = "15/255,24/255,29/255" --export: Screen #4 <hr> Select the background color for screen #4
+Material5 = "TEXT5" --export: Screen #5 <hr> Select the main title for screen #5
+itemID5 = 2125213321 --export: Screen #4 <hr> The item ID for screen #4 <br>ID found at du-lua.dev/#/items
+Value5 = '1' --export: Screen #5 <hr> Select the value you want for screen #5
+ContSize5 = 1500 --export: Screen #5 <hr> The total container size for screen #5
+ReverseRGB5 = true --export: Screen #4 <hr> Select the RGB bar direction for screen #4
+BG_Color5 = "15/255,24/255,29/255" --export: Screen #5 <hr> Select the background color for screen #5
+
+-- Basic options for screen looks
+UseWallpaper = false --export: Use or not use the wallpaper url below
+WallPaper = "assets.prod.novaquark.com/130546/b6fc96cb-c570-4554-901f-099711f5383d.jpg" --export: Wallpapper Image URL
+-- HideUseage = false --export: hides the used volume under the percent bar
+AutoContainer = true --export: Brabs all the container data itself.
+HideUnit = true --export: Hides the bottom right widget
+HideTier = false --export: Hides the items tier in the header
+HideGitHub = false --export: Hides GitHub link credit
+showCount = true --export: Show how many items in container | Cant be used with showValue
+showValue = false --export: Show the total value of your item/items based on values from above
+showJericoBar = true --export: Shows the Header bar (JerichoBar)
+showinKL = false --export: Show storage volume used in KL or L
+showTotal = false --export: Show total next to used amount on screen
+-- showFrame = false --export: Shows a hub outline for placing hubs on screen
+
+-- Backend stuff
+version = '2.5'
